8fadc9df463d231bf819624938a5f76768f3b146
Sourav nainwal <101329625+SouravNainwal@users.noreply.github.com>
2022-06-03 12:01:44 +0530

    Add files via upload

---
+++ Employee.js
@@ -0,0 +1,69 @@
+﻿function getbyID(EmpID) {
+    $('#Name').css('border-color', 'lightgrey');
+    $('#Email').css('border-color', 'lightgrey');
+    $('#PhoneNo').css('border-color', 'lightgrey');
+    $('#Department').css('border-color', 'lightgrey');
+    $.ajax({
+        url: "/Home/getbyID/" + EmpID,
+        typr: "GET",
+        contentType: "application/json;charset=UTF-8",
+        dataType: "json",
+        success: function (result) {
+            $('#Id').val(result.id);
+            $('#Name').val(result.name);
+            $('#Email').val(result.email);
+            $('#PhoneNo').val(result.phoneNo);
+            $('#Department').val(result.department);
+
+            $('#myModal').modal('show');
+            $('#btnUpdate').show();
+            $('#btnAdd').hide();
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+    return false;
+}
+
+
+function Update() {
+    ShowLoading();
+    debugger;
+    var emp = {
+        Id: $('#Id').val(),
+        Name: $('#Name').val(),
+        Email: $('#Email').val(),
+        PhoneNo: $('#PhoneNo').val(),
+        Department: $('#Department').val(),
+    };
+    ClosePopup();
+    $.ajax({
+        url: "/Home/Update",
+        data: emp,
+        type: "POST",
+        success: function (result) {
+            HideLoading();
+            location.reload();
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+}
+
+function ResetFormControl() {
+    $('#Id').val("");
+    $('#Name').val("");
+    $('#Email').val("");
+    $('#PhoneNo').val("");
+    $('#Department').val("");
+}
+
+function ClosePopup() {
+    ResetFormControl();
+    $('#myModal').modal('hide');
+}
+
+function ShowLoading() { $('.spanner').addClass('show'); }
+function HideLoading() { $('.spanner').removeClass('show'); }
